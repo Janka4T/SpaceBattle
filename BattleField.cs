ddfa1ed897e4a9feaca579d150d31355677a85e1
Jānis Tulišs <janistuliss3@gmail.com>
2020-04-10 17:30:38 +0300

    v.17:30

--- BattleField.cs
+++ BattleField.cs
@@ -12,9 +12,56 @@ namespace SpaceBattle
 {
     public partial class Battlefield : Form
     {
+        
+        Spaceship spaceship;
+        Bullet bullet;
+
         public Battlefield()
         {
             InitializeComponent();
+            InitializeBattleField();
+        }
+
+        private void InitializeBattleField()
+        {
+            this.BackColor = Color.Black;
+            spaceship = new Spaceship();
+            spaceship.Left = ClientRectangle.Width - (ClientRectangle.Width / 2 + spaceship.Width / 2);
+            spaceship.Top = 300;
+            this.Controls.Add(spaceship);
+        }
+
+        private void FireBullet()
+        {
+            bullet = new Bullet();
+            bullet.Top = spaceship.Top;
+            bullet.Left = spaceship.Left + (spaceship.Width / 2 - bullet.Width / 2);
+            this.Controls.Add(bullet);
+            bullet.BringToFront();
+        }
+
+        private void Battlefield_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                FireBullet();
+            }
+            else if(e.KeyCode == Keys.A)
+            {
+                spaceship.Left -= 10;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                spaceship.Left += 10;
+            }
+        }
+
+        private void Battlefield_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                FireBullet();
+            }
         }
     }
 }
